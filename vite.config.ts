16f83cda2147d2f84621265650b27a0df6bc3fd6
Dinglberry <122329271+Dinglberry@users.noreply.github.com>
2026-04-26 04:20:34 +0000

    add base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/meal-prep-budget-system/",
   server: {
     host: true,
     allowedHosts: "all"
-  }
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
 });
